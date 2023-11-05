b19dd49e65b65f5e5a0c8d8d5a6422960d6810f3
Enrico Zoratto <122989369+sidts@users.noreply.github.com>
2023-11-05 18:02:31 -0300

    Update Usuario.java

--- src/negocio/Usuario.java
+++ src/negocio/Usuario.java
@@ -6,8 +6,66 @@ public abstract class Usuario {
 	protected String nomeUsuario;
 	protected String email;
 	protected String senha;
+	protected int qtdNotas;
+	protected int qtdPastas;
 	protected Pasta[] pastas;
 	protected int numPastas;
-	protected int qtdPastas;
-	protected int qtdNotas;
+	
+	
+	public Usuario(String n, String d, String nU, String e, String s) {
+		this.nome = n;
+		this.dtNascimento = d;
+		this.nomeUsuario = nU;
+		this.email = e;
+		this.senha = s;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDtNascimento() {
+		return dtNascimento;
+	}
+
+	public void setDtNascimento(String dtNascimento) {
+		this.dtNascimento = dtNascimento;
+	}
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+
+	public String toString() {
+		return "Usuario [nome=" + nome + ", nomeUsuario=" + nomeUsuario + ", qtdNotas=" + qtdNotas + ", qtdPastas="
+				+ qtdPastas + "]";
+	}
+
+
+	
 }
