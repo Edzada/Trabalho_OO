aca1e11bdb6be621c83a1a5dd1e8d6db91745d9b
Enrico Zoratto <122989369+sidts@users.noreply.github.com>
2023-11-05 18:04:29 -0300

    Update UsuarioPlus.java

--- src/negocio/UsuarioPlus.java
+++ src/negocio/UsuarioPlus.java
@@ -2,4 +2,23 @@ package negocio;
 
 public class UsuarioPlus extends Usuario{
 	private Pagamento cartao;
+	
+	public Usuario_Plus(String n, String d, String nU, String e, String s, Pagamento c) {
+		super(n, d, nU, e, s);
+		this.cartao = c;
+		this.qtdNotas = 1500;
+		this.qtdPastas = 1500;
+		this.pastas = new Pasta[1500];
+		this.numPastas = 0;
+		
+	}
+
+	public Pagamento getCartao() {
+		return cartao;
+	}
+
+	public void setCartao(Pagamento cartao) {
+		this.cartao = cartao;
+	}
+	
 }
