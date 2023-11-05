2c9778aec7eaf0f9be73dc5236f47b751fa9526c
Enrico Zoratto <122989369+sidts@users.noreply.github.com>
2023-11-05 18:03:30 -0300

    Update UsuarioFree.java

--- src/negocio/UsuarioFree.java
+++ src/negocio/UsuarioFree.java
@@ -1,5 +1,25 @@
 package negocio;
 
-public class UsuarioFree extends Usuario {
-	private String anuncios;
+public class UsuarioFree extends Usuario{
+	private String anuncio;
+	
+	public Usuario_Free(String n, String d, String nU, String e, String s) {
+		super(n, d, nU, e, s);
+		this.qtdNotas = 50;
+		this.qtdPastas = 50;
+		this.pastas = new Pasta[50];
+		this.numPastas = 0;
+	}
+
+	public String getAnuncio() {
+		return anuncio;
+	}
+
+	public void setAnuncio(String anuncio) {
+		this.anuncio = anuncio;
+	}
+	
+	
+	
+	
 }
