84b6d6308ff8f198e224e6dec265df9298bfdd6a
Enrico Zoratto <122989369+sidts@users.noreply.github.com>
2023-11-05 18:01:45 -0300

    Update Pasta.java
    tem duas variaveis nao declaradas com sets e gets, nem tem metódos

--- src/negocio/Pasta.java
+++ src/negocio/Pasta.java
@@ -3,7 +3,52 @@ package negocio;
 public class Pasta {
 	private String tituloPasta;
 	private String dtCriacaoPasta;
-	private int numNotas;
 	private Nota[] notas;
+	private int numNotas;
+	
+	public Pasta(String t, String d) {
+		this.tituloPasta = t;
+		this.dtCriacaoPasta = d;
+		
+	}
 
+	public String getTituloPasta() {
+		return tituloPasta;
+	}
+
+	public void setTituloPasta(String tituloPasta) {
+		this.tituloPasta = tituloPasta;
+	}
+
+	public String getDtCriacaoPasta() {
+		return dtCriacaoPasta;
+	}
+
+	public void setDtCriacaoPasta(String dtCriacaoPasta) {
+		this.dtCriacaoPasta = dtCriacaoPasta;
+	}
+
+	public Nota[] getNotas() {
+		return notas;
+	}
+
+	public void setNotas(Nota[] notas) {
+		this.notas = notas;
+	}
+
+	public int getNumNotas() {
+		return numNotas;
+	}
+
+	public void setNumNotas(int numNotas) {
+		this.numNotas = numNotas;
+	}
+
+	@Override
+	public String toString() {
+		return "Pasta [tituloPasta=" + tituloPasta + ", dtCriacaoPasta=" + dtCriacaoPasta + ", numNotas=" + numNotas
+				+ "]";
+	}
+	
+	
 }
