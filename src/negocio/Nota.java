f6e2c9db565c8724691444409c7e1aabdb2dc100
Enrico Zoratto <122989369+sidts@users.noreply.github.com>
2023-11-05 17:54:55 -0300

    Update Nota.java

--- src/negocio/Nota.java
+++ src/negocio/Nota.java
@@ -2,9 +2,45 @@ package negocio;
 
 public class Nota {
 	private String tituloNota;
-	private String dtCriacaoNotas;
+	private String dtCriacaoNota;
 	private String conteudoNota;
 	
-	
+	public Nota(String t, String d, String c) {
+		this.tituloNota = t;
+		this.dtCriacaoNota = d;
+		this.conteudoNota = c;
+	}
 
+	public String getTituloNota() {
+		return tituloNota;
+	}
+
+	public void setTituloNota(String tituloNota) {
+		this.tituloNota = tituloNota;
+	}
+
+	public String getDtCriacaoNota() {
+		return dtCriacaoNota;
+	}
+
+	public void setDtCriacaoNota(String dtCriacaoNota) {
+		this.dtCriacaoNota = dtCriacaoNota;
+	}
+
+	public String getConteudoNota() {
+		return conteudoNota;
+	}
+
+	public void setConteudoNota(String conteudoNota) {
+		this.conteudoNota = conteudoNota;
+	}
+
+	public String toString() {
+		return "Nota [tituloNota=" + tituloNota + ", dtCriacaoNota=" + dtCriacaoNota + ", conteudoNota=" + conteudoNota
+				+ "]";
+	}
+
+	
+	
 }
+
