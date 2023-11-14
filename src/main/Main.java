842472fc97768cd8400b767804c6b6e373682751
andre-maia51 <acmc.0410@gmail.com>
2023-11-14 13:21:40 -0300

    fiz o bglh de cadastrar usuario

--- src/main/Main.java
+++ src/main/Main.java
@@ -82,17 +82,17 @@ public class Main {
 	}
 
 	public static String imprimirMenu() {
-		String saida = new String("<<<< MENU >>>>\n");
-		saida = saida + "00 - Sair da aplicativo\n";
-		saida = saida + "01 - Cadastrar novo Usuário\n";
-		saida = saida + "02 - Remover Usuário existente\n";
-		saida = saida + "03 - Editar Usuário existente\n";
-		saida = saida + "04 - Listar Usuários\n";
-		saida = saida + "05 - Filtrar Usuários\n";
-		saida = saida + "06 - Criar uma nova Pasta\n";
-		saida = saida + "07 - Remover uma Pasta existente\n";
-		saida = saida + "08 - Editar Pastas\n";
-		saida = saida + "09 - Listar Pastas\n";
+		String saida = new String("\n<<<< MENU >>>>\n");
+		saida = saida + "0 - Sair da aplicativo\n";
+		saida = saida + "1 - Cadastrar novo Usuário\n";
+		saida = saida + "2 - Remover Usuário existente\n";
+		saida = saida + "3 - Editar Usuário existente\n";
+		saida = saida + "4 - Listar Usuários\n";
+		saida = saida + "5 - Filtrar Usuários\n";
+		saida = saida + "6 - Criar uma nova Pasta\n";
+		saida = saida + "7 - Remover uma Pasta existente\n";
+		saida = saida + "8 - Editar Pastas\n";
+		saida = saida + "9 - Listar Pastas\n";
 		saida = saida + "10 - Filtrar Pastas\n";
 		saida = saida + "11 - Criar Nota\n";
 		saida = saida + "12 - Remover Nota\n";
@@ -100,42 +100,82 @@ public class Main {
 		saida = saida + "14 - Listar Notas\n";
 		saida = saida + "15 - Filtrar Notas\n";
 		saida = saida + "16 - Buscar Notas\n";
-		saida = saida + "-> Escolha uma das opções a seguir: ";
+		saida = saida + ">> Escolha uma das opções: ";
 
 		return saida;
 	}
 
-	public static boolean cadastrarUsuario(){
-		System.out.println("Escolha o plano desejado:\n1 -  Plano Free\n2 - Plano Plus");
+	public static void cadastrarUsuario(){
+		System.out.print("\nEscolha o plano desejado:\n1 - Plano Free\n2 - Plano Plus\n>> ");
 		int n = in.nextInt();
 
 		if(n == 1) {
-			String nome;
-			String dtNascimento;
-			String nomeUsuario;
-			String email;
-			String senha;
+			String nome, dtNascimento, nomeUsuario, email, senha;
 
 			in.nextLine();
 
 			System.out.print("Informe o nome do usuário: ");
 			nome = in.nextLine();
-			System.out.print("\nInforme a data de nascimento do usuário: ");
+			System.out.print("Informe a data de nascimento do usuário: ");
 			dtNascimento = in.nextLine();
-			System.out.print("\nInforme o nome de usuário: ");
+			System.out.print("Informe o nome de usuário: ");
 			nomeUsuario = in.nextLine();
-			System.out.print("\nInforme o email: ");
+			System.out.print("Informe o email: ");
 			email = in.nextLine();
-			System.out.print("\nInforme a senha para finalizar o cadastro: ");
+			System.out.print("Informe a senha para finalizar o cadastro: ");
 			senha = in.nextLine();
 
 			Usuario u = new UsuarioFree(nome, dtNascimento, nomeUsuario, email, senha);
 
+			if(d.getNumUsuarios() < 200) {
+				d.addUsuario(u);
+				System.out.println("Usuário cadastrado com sucesso!");
+			} else {
+				System.out.println("Não é possível cadastrar o Usuário!");
+			}
 
 		} else if(n == 2) {
+			String nome, dtNascimento, nomeUsuario, email, senha, emailFatura, numCartao, validade, pais;
+			int cvv;
+			double limite;
+
+			in.nextLine();
+
+			System.out.print("Informe o nome do usuário: ");
+			nome = in.nextLine();
+			System.out.print("Informe a data de nascimento do usuário: ");
+			dtNascimento = in.nextLine();
+			System.out.print("Informe o nome de usuário: ");
+			nomeUsuario = in.nextLine();
+			System.out.print("Informe o email: ");
+			email = in.nextLine();
+			System.out.print("Informe a senha: ");
+			senha = in.nextLine();
+			System.out.print("Informe o email para o recebimento da fatura da assinatura: ");
+			emailFatura = in.nextLine();
+			System.out.print("Informe o número do cartão: ");
+			numCartao = in.nextLine();
+			System.out.print("Informe a data de validade do cartão: ");
+			validade = in.nextLine();
+			System.out.print("Informe o país onde o cartão foi emitido: ");
+			pais = in.nextLine();
+			System.out.print("Informe o CVV do cartão: ");
+			cvv = in.nextInt();
+			System.out.print("Informe o limite do cartão: ");
+			limite = in.nextDouble();
+
+			Pagamento cartao = new Pagamento(emailFatura, numCartao, validade, cvv, pais, limite);
+			Usuario u = new UsuarioPlus(nome, dtNascimento, nomeUsuario, email, senha, cartao);
+
+			if(d.getNumUsuarios() < 200) {
+				d.addUsuario(u);
+				System.out.println("Usuário cadastrado com sucesso!");
+			} else {
+				System.out.println("Não é possível cadastrar o Usuário!");
+			}
 
 		} else {
-			System.out.print("\nOpção Inválida\n");
+			System.out.print("Opção Inválida\n");
 		}
 
 	}
